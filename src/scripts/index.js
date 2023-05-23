aa488a701b82c0ecc94a2bd84a77bd7d4d811769
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2023-05-23 14:49:27 +0800

    Add stater-project + routes

---
+++ src/scripts/index.js
@@ -0,0 +1,20 @@
+import 'regenerator-runtime';
+
+import '../styles/style.css';
+import '../styles/responsive.css';
+
+import App from './views/app';
+
+const app = new App({
+  header: document.querySelector('#headerContent'),
+  content: document.querySelector('#mainContent'),
+  footer: document.querySelector('#footerContent'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+});
