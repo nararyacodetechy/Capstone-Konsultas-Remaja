29e899ccda29a60b940af89ce7e53d691840b956
DwiAndikaC <K_Dwiandika@outlook.com>
2023-05-30 21:26:10 +0800

    Add Booking-Page + Checkout-Page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,12 +1,17 @@
-import HomePage from '../views/pages/home-page';
-import RegisterPage from '../views/pages/register-page';
-import LoginPage from '../views/pages/login-page';
+import HomePage from "../views/pages/home-page";
+import RegisterPage from "../views/pages/register-page";
+import LoginPage from "../views/pages/login-page";
+
+import BookingPage from "../views/pages/booking-page";
+import CheckoutPage from "../views/pages/checkout-page";
 
 const routes = {
-  '/': HomePage, // default page
-  '/home-page': HomePage,
-  '/register-page': RegisterPage,
-  '/login-page': LoginPage,
+	"/": HomePage, // default page
+	"/home-page": HomePage,
+	"/register-page": RegisterPage,
+	"/login-page": LoginPage,
+	"/booking-page": BookingPage,
+	"/checkout-page": CheckoutPage
 };
 
 export default routes;
