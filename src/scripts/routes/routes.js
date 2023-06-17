6bf3ee5d828cb7334ba7cad68cefb37a3fe11c2f
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2023-06-17 11:11:43 +0800

    Remove Static Login and register page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,6 +1,4 @@
 import HomePage from '../views/pages/home-page';
-import RegisterPage from '../views/pages/register-page';
-import LoginPage from '../views/pages/login-page';
 import ListConsultantPage from '../views/pages/list-consultant-page';
 import DetailConsultantPage from '../views/pages/detail-consultant-page';
 import BookingPage from '../views/pages/booking-page';
@@ -10,8 +8,6 @@ import ArticlePage from '../views/pages/arcticle-page';
 const routes = {
   '/': HomePage, // default page
   '/home-page': HomePage,
-  '/register-page': RegisterPage,
-  '/login-page': LoginPage,
   '/list-consultant-page': ListConsultantPage,
   '/detail-consultant-page': DetailConsultantPage,
   '/booking-page': BookingPage,
