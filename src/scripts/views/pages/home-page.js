3f161f9099b6eb012a2a8816c29dc90c118a0dc1
Razif Ilham Rosadi <razifilham.12@gmail.com>
2023-06-18 21:02:17 +0700

    memindahkan gambar about

--- src/scripts/views/pages/home-page.js
+++ src/scripts/views/pages/home-page.js
@@ -89,6 +89,7 @@ const HomePage = {
             <div class="row">
                 <div
                     class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch position-relative">
+                    <img src="./images/home-page/about/group.png" alt="Group Image">
                 </div>
                 <div
                     class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
