d55d81d331821dc91b39a239333f3580cbb5adbb
Iqbali18 <iqbalviqi5@gmail.com>
2023-06-19 15:59:26 +0700

    mengganti paragraf bagian none

--- src/scripts/views/pages/none-page.js
+++ src/scripts/views/pages/none-page.js
@@ -5,7 +5,7 @@ const NonePage = {
           <div class="container none-page">
              <img src="https://static.sehatq.com/monorepo-cdnassets/prod/public/images/empty.svg">
              <div class="description-none-found">
-                <h3>Halaman yang Kamu Cari Tidak Ditemukan</h3>
+                <h3>Halaman yang kamu cari tidak ditemukan!</h3>
                 <a href="/">Kembali ke Beranda</a>
              </div>
           </div>
