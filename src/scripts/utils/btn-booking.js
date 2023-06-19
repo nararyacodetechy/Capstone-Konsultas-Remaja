b06f631bb36c4e8574ddeb4664479502fba9bdf7
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2023-06-19 19:39:41 +0800

    Add menu-bar for navbar in mobile show

--- src/scripts/utils/btn-booking.js
+++ src/scripts/utils/btn-booking.js
@@ -1,10 +1,7 @@
-
-const test =  document.querySelector("#test")
-const btn = document.querySelector(".btn")
-btn.addEventListener('click', ()=>{
-   test.style.backgroundColor="red";
-   btn.classList.add('active');
+const test = document.querySelector('#test');
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  test.style.backgroundColor = 'red';
+  btn.classList.add('active');
 });
-document.getElementById("btn0").click();
-
-
+document.getElementById('btn0').click();
